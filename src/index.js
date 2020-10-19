e96c90e5cfa2f2cf7670323a33967a92e67350ac
Tauan Gabriel <tauan@alunos.utfpr.edu.br>
2020-10-19 15:50:08 -0300

    Subindo o projeto zerado (Backup)

---
+++ src/index.js
@@ -0,0 +1,11 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const App = (props) => (
+  <View>
+    <Text>Pagina inicial - mobile</Text>
+  </View>
+);
+
+export default App;
